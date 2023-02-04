910c80295b0e0f348c780979fe3d84471a45c9a8
Caleb Swartz <100005685+calebjswartz@users.noreply.github.com>
2023-02-04 16:57:19 -0600

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+cmc = requests.get(f"https://coinmarketcap.com/currencies/ethereum")
+soup = BeautifulSoup(cmc.content, 'html.parser')
+data = soup.find('script', id="__NEXT_DATA__", type="application/json")
+coin_data = json.loads(data.contents[0])
+
+info = coin_data['props']['quotesLatestData']
+eth_data = next(item for item in info if item["symbol"] == "ETH")
+price = eth_data['p']
+
+print("The current price of ethereum is " + price)
